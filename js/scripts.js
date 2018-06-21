25381022a08b9d3d520cef1d84d4ae3a8feb626d
LUIS-PC\LUIS <info@aliasdev.com>
2018-06-21 23:15:13 +0100

    Significant progress on all files

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,20 @@
 
 jQuery(document).ready(function(){
 	
+	// DYNAMIC CONTAINER HEIGHTS
+	function resizeContainers() {
+		var toolbarHeight = $("#toolbar").outerHeight();
+		var windowHeight = $(window).height();
+		$("#main-container").css("margin-top",toolbarHeight);
+		$("#sidebar").outerHeight( windowHeight - toolbarHeight );
+	}
+	
+	resizeContainers();
+	
+	$(window).resize(function(){
+		resizeContainers();
+	});
+	
+	
+	
 });
