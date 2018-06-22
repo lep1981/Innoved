cf5085591c9ea06065d79bcf6621be6ab5baf3b7
LUIS-PC\LUIS <info@aliasdev.com>
2018-06-22 01:49:18 +0100

    Final progress

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,33 @@
 
 jQuery(document).ready(function(){
 	
-	// DYNAMIC CONTAINER HEIGHTS
+	// LOAD TAB CONTENT
+	$("#tab-content").load("tab-scores-content.html", function(){
+		$(".tab-info:first").slideDown("fast");
+		
+		// TAB FUNCTIONALITY
+		$(".tab-header").click(function(){
+			$(this).next(".tab-info").slideToggle("fast");
+		});
+		
+		$(".expand-all-tabs").click(function(){
+			$(".tab-info").slideDown("fast");
+		});
+		
+		$(".collapse-all-tabs").click(function(){
+			$(".tab-info").slideUp("fast");
+		});
+	});
+	
+	
+	
+	// DYNAMIC CONTAINER WIDTHS & HEIGHTS
 	function resizeContainers() {
 		var toolbarHeight = $("#toolbar").outerHeight();
 		var windowHeight = $(window).height();
+		var sidebarWidth = $("#sidebar").outerWidth();
 		$("#main-container").css("margin-top",toolbarHeight);
+		$("#main-content").css("margin-left",sidebarWidth - 10);
 		$("#sidebar").outerHeight( windowHeight - toolbarHeight );
 	}
 	
